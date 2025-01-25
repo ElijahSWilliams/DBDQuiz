0a501cda3e121b28594149f6800c2b33abc819fe
Elijah <xrealntrue16x@gmail.com>
2025-01-25 17:36:41 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <h1>
+      DBB Quiz: To Do: Add Killer and Survivor Icons to dtermine what the quiz
+      will be on, Maybe A difficulty option, score counter, a timer, possible
+      sound effects
+    </h1>
+  );
+}
+
+export default App;
