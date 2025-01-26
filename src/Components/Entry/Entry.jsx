4e4c15a52682a3424236341d040495903be68de6
Elijah <xrealntrue16x@gmail.com>
2025-01-25 19:26:27 -0500

    Skill Check SOund Added to Start

--- src/Components/Entry/Entry.jsx
+++ src/Components/Entry/Entry.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState, useRef } from "react";
 import lullaby from "../../assets/huntressLullaby.mp3";
+import drLaugh from "../../assets/drLaugh.mp3";
+import skillCheck from "../../assets/dbdSkillCheck.mp3";
 import "./Entry.css";
 
 const Entry = () => {
   const [isPlaying, setIsPlaying] = useState(false); //state for audio, set to false initially
   const audioRef = useRef(null); // audioRef is used to hold a reference to the <audio> object
   //so you can control it (play, pause, etc.) directly without causing re-renders of the component.
+  const startBtnAudioRef = useRef(null);
 
   useEffect(() => {
     audioRef.current = new Audio(lullaby); //create audio object and assign it to audioRef.current
+
+    startBtnAudioRef.current = new Audio(
+      skillCheck
+    ); /*  create audio object and assn it to startBtnAudioRef.current*/
   }, []);
 
   const handlePlay = () => {
@@ -19,7 +26,8 @@ const Entry = () => {
       audioRef.current
         .play() //play audio
         .then(() => {
-          setIsPlaying(true); //set isPlaying to true (update state)
+          setIsPlaying(true);
+          //set isPlaying to true (update state)
         })
         .catch((err) => {
           console.error(err); //catch errors
@@ -27,13 +35,36 @@ const Entry = () => {
     }
   };
 
+  const handleStartAudio = () => {
+    //check if audio exists
+    if (startBtnAudioRef.current) {
+      startBtnAudioRef.current.currentTime = 0.2;
+      startBtnAudioRef.current
+        .play()
+        .then(() => {})
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  };
+
+  const handleStartQuiz = () => {
+    handleStartAudio(); //call audio function
+
+    //start quiz
+  };
+
   return (
     <div className="entry">
       <h1 className="entry__header">Welcome to the DBD Quiz</h1>
       <button onClick={handlePlay} disabled={isPlaying}>
+        {" "}
+        {/* set btn text based on isPlaying value */}
         {isPlaying ? "La La La La" : "Muted"}
       </button>
-      <button className="entry__play-btn">Enter</button>
+      <button onClick={handleStartQuiz} className="entry__play-btn">
+        Enter
+      </button>
     </div>
   );
 };
